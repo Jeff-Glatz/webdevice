fdf4d476790bc939fcaba5abd1da743cbbba111b
Jeff Glatz <jeff@kungzhi.com>
2020-01-01 11:16:23 -0600

    Add @Inherited

--- src/main/java/io/webdevice/wiring/EnableWebDevice.java
+++ src/main/java/io/webdevice/wiring/EnableWebDevice.java
@@ -3,15 +3,17 @@ package io.webdevice.wiring;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+@Inherited
+@Documented
 @Target(TYPE)
 @Retention(RUNTIME)
-@Documented
 @Import(WebDeviceWiring.class)
 public @interface EnableWebDevice {
 }
