27210725f49ce4f2d174d2d1049abe84fe7df6f8
Jeff Glatz <jeff@kungzhi.com>
2020-01-10 20:28:10 -0600

    Attempt release plugin usage

--- deploy/release.sh
+++ deploy/release.sh
@@ -2,17 +2,9 @@
 echo "Releasing ${TRAVIS_TAG}"
 # Switch to a non-detached branch
 git switch -c release/${TRAVIS_TAG}
-# Set the version to build
-mvnw versions:set -e -B -ntp -DnewVersion=${TRAVIS_TAG} -DgenerateBackupPoms=false
-# Build, install, test then publish the artifacts
-mvnw clean deploy -e -B -ntp -settings .mvn/settings.xml -P ossrh
-# Commit release changes
-git add pom.xml
-git commit --message "Release ${TRAVIS_TAG} (build: ${TRAVIS_BUILD_NUMBER})"
-# Set the next development snapshot version
-mvnw release:update-versions -e -B -ntp -P ossrh
-# Commit the changes
-git add pom.xml
-git commit --message "Next development version (build: ${TRAVIS_BUILD_NUMBER})"
+# Prepare the release
+mvnw release:prepare -e -B -ntp -settings .mvn/settings.xml -P ossrh -Dtag=${TRAVIS_TAG} -DreleaseVersion=${TRAVIS_TAG}
+# Stage the release
+mvnw release:stage -e -B -ntp -settings .mvn/settings.xml -P ossrh
 # Push changes to origin
 git push -u ssh-origin master
