a9de27c9701adc935bed63e227fe37f726a16501
Jeff Glatz <jeff@kungzhi.com>
2020-01-10 21:04:48 -0600

    Simplify release script

--- deploy/release.sh
+++ deploy/release.sh
@@ -3,6 +3,4 @@ echo "Releasing ${TRAVIS_TAG}"
 # Prepare the release
 mvnw -e -B -ntp -settings .mvn/settings.xml -P ossrh -Dtag=${TRAVIS_TAG} -DreleaseVersion=${TRAVIS_TAG} release:prepare
 # Stage the release
-mvnw -e -B -ntp -settings .mvn/settings.xml -P ossrh release:stage
-# Push changes to origin
-git push -u ssh-origin master
+mvnw -e -B -ntp -settings .mvn/settings.xml -P ossrh release:perform
