6f5c863bbfd47c0f94df8537628c92f28f4975f9
Jeff Glatz <jeff@kungzhi.com>
2020-01-18 04:03:42 -0600

    Removed script

--- deploy/release-docs.py
+++
@@ -1,9 +0,0 @@
-import fileinput
-import os
-import re
-
-version = os.environ["TRAVIS_TAG"]
-
-for line in fileinput.input("docs/index.md", inplace=True):
-    print(re.sub("(<version>).*(</version>)", f"\g<1>{version}\g<2>",
-                 line.rstrip(), flags=re.DOTALL))
