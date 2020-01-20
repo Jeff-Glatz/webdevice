0d36519c303d907d473063dc3c47e157f8e50405
Jeff Glatz <jeff@kungzhi.com>
2020-01-20 13:35:03 -0600

    Update deploy scripts to upload code coverage reports for snapshot and release builds

--- deploy/release-tag.sh
+++ deploy/release-tag.sh
@@ -17,6 +17,7 @@ echo "Setting POM versions to ${TRAVIS_TAG}"
 mvn -e -B -ntp -s deploy/settings.xml -P ossrh -DgenerateBackupPoms=false -DnewVersion=${TRAVIS_TAG} versions:set
 
 echo "Executing deploy goal for ${TRAVIS_TAG}"
+# TODO: abort on failure to deploy due to staging rule violations
 mvn -e -B -ntp -s deploy/settings.xml -P ossrh clean deploy
 
 echo "Updating version references in documentation"
@@ -28,6 +29,9 @@ git add docs/
 find . -name pom.xml -exec git add {} \;
 git commit -m "Release ${TRAVIS_TAG} (build: ${TRAVIS_BUILD_NUMBER})"
 git push -u origin release/${TRAVIS_TAG}
+
+echo "Uploading code coverage report for ${TRAVIS_TAG}"
+bash <(curl -s https://codecov.io/bash)
 
 echo "Merging release/${TRAVIS_TAG} into master"
 git checkout master
