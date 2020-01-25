17039ac5962fe6e1f0ef8d7caa6021472e226db7
Jeff Glatz <jeff@kungzhi.com>
2020-01-25 07:04:27 -0600

    Remove todo

--- framework/src/main/java/io/webdevice/device/WebDevice.java
+++ framework/src/main/java/io/webdevice/device/WebDevice.java
@@ -247,7 +247,6 @@ public class WebDevice
 
     @Override
     public void quit() {
-        // TODO: Move release() here
         log.warn("Only the provider of the current device should manage the driver's lifecycle");
     }
 
