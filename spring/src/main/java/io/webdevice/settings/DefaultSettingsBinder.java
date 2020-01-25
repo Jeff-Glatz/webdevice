b2485f0406f1dd3553a06244335d4649b97958c0
Jeff Glatz <jeff@kungzhi.com>
2020-01-25 14:39:18 -0600

    Dial down logging

--- spring/src/main/java/io/webdevice/settings/DefaultSettingsBinder.java
+++ spring/src/main/java/io/webdevice/settings/DefaultSettingsBinder.java
@@ -11,23 +11,25 @@ import static io.webdevice.wiring.WebDeviceScope.namespace;
 import static java.util.Arrays.stream;
 import static org.springframework.util.StringUtils.capitalize;
 
-// TODO: Needs work
 public class DefaultSettingsBinder
         implements SettingsBinder {
     private final Logger log = LoggerFactory.getLogger(DefaultSettingsBinder.class);
 
     @Override
     public Settings from(ConfigurableEnvironment environment) {
-        String prefix = namespace("");
         Settings settings = new Settings();
         BeanWrapper wrapper = new BeanWrapperImpl(settings);
         wrapper.setAutoGrowNestedPaths(true);
+        wrapper.setExtractOldValueForEditor(true);
+        String prefix = namespace("");
         environment.getPropertySources().stream()
+                // Collect all webdevice specific properties
                 .filter(EnumerablePropertySource.class::isInstance)
                 .map(EnumerablePropertySource.class::cast)
                 .flatMap(source -> stream(source.getPropertyNames()))
                 .filter(name -> name.startsWith(prefix))
                 .distinct()
+                // Apply each property to the settings
                 .forEach(environmentProperty -> {
                     String property = toCamelCase(environmentProperty.substring(prefix.length()));
                     String value = environment.getProperty(environmentProperty);
@@ -38,13 +40,12 @@ public class DefaultSettingsBinder
     }
 
     private static String toCamelCase(String value) {
-        return stream(value.split("-"))
-                .collect(StringBuilder::new,
-                        (builder, part) -> builder.append(builder.length() == 0
-                                ? part :
-                                capitalize(part)),
-                        (a, b) -> {
-                        })
-                .toString();
+        StringBuilder builder = new StringBuilder();
+        for (String part : value.split("-")) {
+            builder.append(builder.length() == 0
+                    ? part :
+                    capitalize(part));
+        }
+        return builder.toString();
     }
 }
